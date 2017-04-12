604e3fdcd0fada147c7fe8c3b6065efa33dc70f2
Anatoli Lipiäinen <toli@Anatolis-MacBook-Pro.local>
2017-04-12 12:20:48 +0300

    Initial switch to omxplayer

--- routes/media.js
+++ routes/media.js
@@ -4,14 +4,28 @@ var router = express.Router();
 var MPlayer = require('mplayer');
 var player = new MPlayer();
 
+var omxp = require('omxplayer-controll');
+var opts = {
+  'audioOutput': 'hdmi', //  'hdmi' | 'local' | 'both'
+  'blackBackground': false, //false | true | default: true
+  'disableKeys': true, //false | true | default: false
+  'disableOnScreenDisplay': false, //false | true | default: false
+  'disableGhostbox': false, //false | true | default: false
+  'subtitlePath': '', //default: ""
+  'startAt': 0, //default: 0
+  'startVolume': 1.0 //0.0 ... 1.0 default: 1.0
+};
+
 /* GET users listing. */
 router.get('/play', function(req, res, next) {
-  player.openFile('uploads/1.tmp');
+  //player.openFile('uploads/1.tmp');
+  omxp.open('uploads/1.tmp', opts);
   res.status(200).end();
 });
 
 router.get('/pause', function(req, res, next) {
-  player.pause();
+  //player.pause();
+  omxp.playPause();
   res.status(200).end();
 });
 
